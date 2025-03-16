d4463e887c50fc6ecb3dce2dae7bd8eac239f048
njongefred9 <frednjonge9@gmail.com>
2025-03-16 15:15:41 +0300

    Add failing validation test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe User, type: :model do
   it 'has a valid factory' do
     expect(user).to be_valid
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
 end
