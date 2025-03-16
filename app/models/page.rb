8451187d28edb74044b03d773ad24ac368bd2267
njongefred9 <frednjonge9@gmail.com>
2025-03-16 17:35:35 +0300

    Setup a page slug
    Add a before_validation method that makes a page slug and test it.

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,4 +3,19 @@ class Page < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true, uniqueness: true
+
+  before_validation :make_slug
+
+  private
+
+  def make_slug
+    return if title.blank?
+
+    self.slug = title
+                .downcase
+                .gsub(/[_ ]/, "-")      # Replace spaces and underscores with single dashes
+                .gsub(/[^a-z0-9-]/, "")  # Remove all special characters except dashes
+                .gsub(/-{2,}/, "-")      # Replace multiple dashes with a single dash
+                .gsub(/^-+|-+$/, "")     # Remove leading and trailing dashes
+  end
 end
--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :page do
     user { create :user }
-    title { "MyString" }
-    slug { "MyString" }
-    summary { "MyText" }
-    content { "MyText" }
+    sequence(:title) { |n| "Page Title #{n}" }
+    sequence(:slug) { |n| "page-title-#{n}" }
+    sequence(:summary) { |n| "<p>Page Summary #{n}</p>" }
+    sequence(:content) { |n| "<p>Page Content #{n}</p>" }
+    created_at { Time.zone.now }
     published { false }
   end
 end
